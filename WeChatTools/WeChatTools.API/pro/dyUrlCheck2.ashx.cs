0c6d8f6e112484e5725cc88567437e21f48e236f
gemgin <23352428+gemgin@users.noreply.github.com>
2022-03-09 13:52:24 +0800

    优化代码

--- WeChatTools/WeChatTools.API/pro/dyUrlCheck2.ashx.cs
+++ WeChatTools/WeChatTools.API/pro/dyUrlCheck2.ashx.cs
@@ -1,11 +1,5 @@
 ﻿
-using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net;
-using System.Text;
 using System.Web;
 using WeChatTools.Core;
 
--- WeChatTools/WeChatTools.API/pro/qqUrlCheck2.ashx.cs
+++ WeChatTools/WeChatTools.API/pro/qqUrlCheck2.ashx.cs
@@ -1,10 +1,4 @@
-﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net;
-using System.Text;
+﻿using System;
 using System.Web;
 using WeChatTools.Core;
 
